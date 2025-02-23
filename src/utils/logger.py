ee6e70c7aa69fc09dbcedcc38a15657257974edb
khadkaashish <ashishkhadka03@gmail.com>
2025-02-23 12:01:25 -0500

    logger.py:removed the duplicate log printed in CLI

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,16 +1,21 @@
 import logging
-import sys
+
 
 def setup_logger():
     logger = logging.getLogger('bus_reservation')
-    logger.setLevel(logging.INFO)
-    
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    
-    logger.addHandler(handler)
-    
-    return logger 
+
+    if not logger.hasHandlers():  # Prevent duplicate handlers
+        logger.setLevel(logging.INFO)
+
+        # Logs will be saved to 'bus_reservation.log' instead of appearing in CLI
+        handler = logging.FileHandler("bus_reservation.log")
+        handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
+
+    logger.propagate = False  # Prevent duplicate logging
+    return logger
